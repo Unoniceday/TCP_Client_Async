ecbadba215b5f4c325e87a3be494a812254ca24b
Uno <uno.niceday@gmail.com>
2019-11-26 18:21:15 +0800

    upload project

---
+++ TCPClient_Async/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCPClient_Async
+{
+    class Message
+    {
+
+        public static byte[] GetBytes(string data)
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            int dataLength = dataBytes.Length;
+
+            //組合成新的數組，組成[數組長度+資料]
+            byte[] lengthBytes = BitConverter.GetBytes(dataLength);
+            byte[] newBytes = lengthBytes.Concat(dataBytes).ToArray();
+            return newBytes;
+        }
+
+    }
+}
---
+++ TCPClient_Async/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5566));
+
+            byte[] data = new byte[1024];
+            int count = clientSocket.Receive(data);
+            string msg = Encoding.UTF8.GetString(data, 0, count);
+
+            while (true)
+            {
+                string s = Console.ReadLine();
+                clientSocket.Send(Message.GetBytes(s));
+                
+            }
+            
+        }
+    }
+}
